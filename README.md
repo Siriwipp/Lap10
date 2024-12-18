c39fd4bc4b87199b674275a4a703f84659c4b0db
Siriwipp <Siriwiphaauddee.mew@gmail.com>
2024-12-18 18:01:05 +0700

    Lab10

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME:ศิริวิภา อวดดี
+# STUDENT ID:670612131
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include<fstream>
+using namespace std;
+
+int main () {
+    int count = 0;
+    float sum = 0;
+    float num = 0;
+
+    string textline;
+    ifstream source("score.txt");
+    while (getline(source,textline)){
+        sum += atof(textline.c_str());
+        num += pow(atof(textline.c_str()),2);
+        count++; 
+    }
+    cout << "Number of data = "<< count <<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<< sum/count <<"\n";
+    cout << "Standard deviation = "<< sqrt((num/count)-pow((sum/count),2)) ;
+}
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
